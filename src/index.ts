98e877a5bdc500769bd88eefa97ebbde03051c89
Oscar Escorcia <oe319840@gmail.com>
2025-11-05 11:27:33 -0500

    feat: Added cors

--- src/index.ts
+++ src/index.ts
@@ -4,6 +4,7 @@ import productsRouter from "./routes/products.routes";
 import authRouter from "./routes/auth.routes";
 import { Database } from "./core/database";
 import { UsersRepository } from "./repositories/users.repository";
+import cors from "cors";
 
 const app = express();
 const PORT = process.env.PORT || 3000;
@@ -13,6 +14,14 @@ const db = new Database("../db/db.json");
 const usersRepo = new UsersRepository(db);
 usersRepo.ensurePasswords("Password123!");
 
+const corsOptions = {
+  origin: ['http://localhost:3000', 'http://localhost:4000'],
+  methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
+  allowedHeaders: ['Content-Type', 'Authorization'],
+  credentials: true
+};
+
+app.use(cors(corsOptions));
 app.use(express.json());
 
 // Routes
@@ -24,7 +33,7 @@ app.get("/", (req: Request, res: Response) => {
 });
 
 app.listen(PORT, () => {
-  console.log('🛍️  E-commerce API Server');
+  console.log('E-commerce API Server');
   console.log('=======================');
   console.log(` Server running on: http://localhost:${PORT}`);
   console.log(' Available endpoints:');
